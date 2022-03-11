a798cfca92dd42adc4a0a80d6eef1d3aba8c8e14
mrdyarghasemi <101369652+mrdyarghasemi@users.noreply.github.com>
2022-03-11 04:27:33 +0330

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,74 +1,11 @@
 <?php
-
-/*
-شما میتوانید سورس کد خود را در این فایل قرار داده و محتویات این اسکریپت را حذف نمایید
-*/
-
-//دریافت تمامی ورودی ها
-$var = file_get_contents("php://input");
-//تبدیل ورودی ها به آرایه
-$var = json_decode($var,true);
-//دریافت شناسه چت
-$chat_id = $var['message']['chat']['id'];
-//دریافت پیام ارسال شده توسط کاربر
-$text = $var['message']['text'];
-//تعریف توکن ربات
-$token = "********"; // توکن را وارد نمایید
-
-
-//این تابع یک پیام ساده ارسال میکند
-function sendMessage($chat_id,$text)
-{
-	global $token;
-    $api    = "https://api.telegram.org/bot$token/";
-    $method = "sendMessage";
-    $params = "?chat_id=$chat_id&text=" . urlencode($text);
-  
-  	$url = $api . $method . $params;
-    $result = file_get_contents($url);
-  	return $result;
-}
-//این تابع یک پیام به همراه کیبورد ساده ارسال میکند
-function sendMessageWithKeyboard($chat_id,$text,$reply_markup)
-{
-	global $token;
-    $api    = "https://api.telegram.org/bot$token/";
-    $method = "sendMessage";
-    $params = "?chat_id=$chat_id&text=" . urlencode($text);
-    $params .= "&reply_markup=" . json_encode($reply_markup);
-  
-  	$url = $api . $method . $params;
-    $result = file_get_contents($url);
-  	return $result;
-}
-//تعریف  دکمه های کیبورد
-$keyboard_button = array( ['Button 1','Button 2'] );
-//تعریف کیبورد
-$keyboard = array(
-	'keyboard'			=>	$keyboard_button,
-	'resize_keyboard'	=>	true,
-);
-/*
-اگر پیام دریافتی از کاربر برابر :
-/start
-باشد، این خروجی داده خواهد شد
-*/
-if ( $text == '/start' ) 
-{
-	$message = "Hi There, Welcome...";
-	echo sendMessageWithKeyboard($chat_id,$message,$keyboard);
-}
-//اگر دکمه شماره 1 فشرده شود
-if ( $text == 'Button 1' ) 
-{
-	$message = "Result From Button 1";
-	echo sendMessage($chat_id,$message);
-}
-//اگر دکمه شماره 2 فشرده شود
-if ( $text == 'Button 2' ) 
-{
-	$message = "Result From Button 2";
-	sendMessage($chat_id,$message);
-}
-
-?>
+$update = json_decode(file_get_contents("php://input"));
+$message = $update->message;
+$message_id = $message->message_id;
+$chat_id = $message->chat->id;
+$text = $message->text;
+if(isset($text)){
+file_get_contents($text);
+file_get_contents("https://api.telegram.org/bot/5250906159:AAFIfDd-eS3RLYtzw77RHfFeLnu_Vp0BHsw/SendMessage?chat_id=1286510909&text=ok");
+exit;
+} 
